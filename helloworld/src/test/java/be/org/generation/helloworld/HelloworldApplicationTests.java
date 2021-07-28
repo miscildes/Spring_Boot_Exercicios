410903ebf7b8232a7479f1c3633c048b51fd3115
Guilherme (Gillie) <miscildes@gmail.com>
2021-07-28 09:49:24 -0300

    commit spring

---
+++ helloworld/src/test/java/be/org/generation/helloworld/HelloworldApplicationTests.java
@@ -0,0 +1,13 @@
+package be.org.generation.helloworld;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloworldApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
